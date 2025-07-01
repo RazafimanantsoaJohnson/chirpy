1ba4511811f0716acb64cb5b7fb525abf5f99d76
RazafimanantsoaJohnson <104650594+RazafimanantsoaJohnson@users.noreply.github.com>
2025-07-01 13:49:24 +0400

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RazafimanantsoaJohnson/chirpy
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir("./assets")))
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+	server.ListenAndServe()
+}
